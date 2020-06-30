38c7cb60cf6041e4fa9685e4bbc61ac83e2fce80
asiekmurek <aaaa@o2.pl>
2020-06-30 18:33:13 +0200

    Add Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Navbar() {
+    return (
+        <nav class="navbar navbar-light bg-light">
+            <a class="navbar-brand" href="#">
+                    Joanna Murawska
+             </a>
+        </nav>
+    );
+}
