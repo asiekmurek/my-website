600cc32418ac6c7b7d4492e7eedb54da3ab325c2
asiekmurek <aaaa@o2.pl>
2020-06-30 21:48:45 +0200

    Add footer

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Navbar({name}) {
     return (
-        <nav className="navbar navbar-light bg-light">
+        <nav className="navbar navbar-dark bg-dark">
             <a className="navbar-brand navbar-header" href="#">
                   {name}
              </a>
