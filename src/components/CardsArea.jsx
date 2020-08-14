5fe9db98b296bf76ecf319c3c077fcf4bb9384ec
asiekmurek <aaaa@o2.pl>
2020-08-14 12:22:33 +0200

    Add new project components lifecycle card

--- src/components/CardsArea.jsx
+++ src/components/CardsArea.jsx
@@ -17,6 +17,13 @@ export default function CardsArea() {
     const randomStuffHerokuLink = "https://agile-cliffs-85240.herokuapp.com/?fbclid=IwAR3WyBZqSmEjJkvSj_vVfNe2HNP5BE5tsZNgY8kY8rnSi_98P_w3Lps9V8Y";
     const randomStuffRepo = "https://github.com/asiekmurek/random-stuff";
 
+    const componentsLifecycleTitle = "Components Lifecycle";
+    const componentLifecycleContent = "Simple app describing components lifecycle. Book by Kirupa Chinnathambi 'React and Redux' was inspired to create this app";
+    const btnComponentLifecycleWebsite = "Website";
+    const btnComponentLifecycleGithub = "GitHub";
+    const componentLifecycleLink = "https://components-lifecycle.vercel.app/";
+    const componentLifecycleRepo = "https://github.com/asiekmurek/components-lifecycle";
+
 
     return (
         <div className="row">
@@ -33,6 +40,14 @@ export default function CardsArea() {
                 repoLink={randomStuffRepo}
                 btnWebsite={btnRandomStuffHerokuPlayGround}
                 btnGithub={btnRandomStuffGithub}
+            />
+
+            <Card title={componentsLifecycleTitle}
+                content={componentLifecycleContent}
+                websiteLink={componentLifecycleLink}
+                repoLink={componentLifecycleRepo}
+                btnWebsite={btnComponentLifecycleWebsite}
+                btnGithub={btnComponentLifecycleGithub}
             />
            
         </div>
